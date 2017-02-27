6f45fb61b16c3a7dac2cfa2627faacc042e44f80
Yael <yaelst@gmail.com>
2017-02-27 18:05:52 +0100

    partialy working

--- scripts/client.js
+++ scripts/client.js
@@ -5,7 +5,7 @@ var chatApp = angular.module('chatApp', ['ngRoute']);
 
 chatApp.service( 'deepstream', function() {
 
-  return deepstream( 'wss://154.dsh.cloud?apiKey=678bd5dd-1700-4f8a-8e35-cd1552d4576c' )
+  return deepstream( 'wss://154.deepstreamhub.com?apiKey=639064d0-1d59-4075-bf11-d25535c91f45')
 
 })
 chatApp.service( 'bindFields', function(){
@@ -38,6 +38,7 @@ chatApp.controller('main',
 function($scope, deepstream, bindFields, $http) {
 
   $scope.logedIn = false;
+  $scope.private = false;
   var usersList = [];
   $scope.login = function() {
     console.log($scope.email, $scope.password)
@@ -70,9 +71,6 @@ function($scope, deepstream, bindFields, $http) {
           var userId = 'user/' + data.id;
           $scope.userId = userId;
           var list = deepstream.record.getList('users');
-          // $scope.list = list;
-          // $scope.list.subscribe(entries);
-
           list.whenReady(()=>{
             if(list.getEntries().indexOf(userId)===-1) {
               list.addEntry(userId);
@@ -95,13 +93,61 @@ function($scope, deepstream, bindFields, $http) {
                 }
               });
             }
-
             list.on('entry-added', addUser);
             list.getEntries().forEach(addUser);
           })
         }
-
       })
+    }
+
+    $scope.setPrivate = function(event) {
+      $scope.private = true;
+      $scope.messages = [];
+      var messages = [];
+      $scope.chatFriend = {
+        id:event.target.id,
+        email:event.target.innerHTML
+      }
+        var chatName = [$scope.userId, $scope.chatFriend.id].sort().join('::');
+        var chatList = deepstream.record.getList(chatName);
+        $scope.chatList = chatList;
+        chatList.subscribe((data)=> {
+          $scope.chatList = data;
+        })
+        chatList.whenReady(()=>{
+          chatList.getEntries().forEach(function(recordName) {
+            var obj = {};
+            var rec = deepstream.record.getRecord(recordName);
+            rec.whenReady(()=>{
+              obj = {
+                id: rec.name,
+                message: rec.get('message'),
+                from: rec.get('from'),
+                to: rec.get('to')
+              }
+              messages.push(obj);
+              $scope.messages = messages;
+              if (!$scope.$$phase) {
+                $scope.$apply()
+              }
+            })
+          })
+        })
+        $scope.submit = function() {
+          var messageId = deepstream.getUid();
+          deepstream.record.getRecord(messageId).set({
+            message: $scope.newMessage,
+            from: $scope.email,
+            to: $scope.chatFriend.email
+          })
+          $scope.messages.push({
+            message: $scope.newMessage,
+            from: $scope.email,
+            to: $scope.chatFriend.email
+          })
+          chatList.addEntry(messageId);
+          $scope.newMessage = '';
+        }
     }
 
   });
